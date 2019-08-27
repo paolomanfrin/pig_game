c7b6cc68f5947476e136eaf200dcc90d13a41a4c
paolomanfrin <paolo@manfrin.ch>
2019-08-27 03:00:11 -0700

    variable renaming

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ GAME RULES:
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
+- The first player to reach 20 points on GLOBAL score wins the game
 */
 
 var scores, roundScore, activePlayer, gameOver;
@@ -21,8 +21,9 @@ function rollDice(){
         return;
     
     var dice = Math.floor(Math.random()*6+1);
-    document.querySelector('.dice').src = "dice-" + dice + ".png";
-    document.querySelector('.dice').style.display = "block";
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.src = "dice-" + dice + ".png";
+    diceDOM.style.display = "block";
     
     if (dice!==1){
         var currentScore = parseInt(document.getElementById('current-' + activePlayer).innerHTML);
@@ -49,12 +50,12 @@ function holdScore() {
     
     
     totalScore = totalScore + currentScore;
-    document.getElementById('score-' + activePlayer).innerHTML = totalScore;
+    document.getElementById('score-' + activePlayer).textContent = totalScore;
     scores[activePlayer] = totalScore; // save score
     
     if (totalScore > 20){
         // a winner 
-        document.getElementById('name-' + activePlayer).innerHTML = "WINNER!";
+        document.getElementById('name-' + activePlayer).textContent = "WINNER!";
         console.log("winner!");
         gameOver = true;
     }
